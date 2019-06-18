f7835a2e5b6335de34d8b658d851d1cafa7a4000
Darshan-Lakade <darshanlakade75@gmail.com>
2019-06-18 19:23:02 +0530

    Updates

---
+++ precache-manifest.62d1e5d30e52f02fb5bb271cca32594a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9c521e234a16319627acd4ce0c8bfa2",
+    "url": "/Robofriends/./index.html"
+  },
+  {
+    "revision": "cf21ad94375f54112280",
+    "url": "/Robofriends/./static/css/main.6c94881d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Robofriends/./static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cf21ad94375f54112280",
+    "url": "/Robofriends/./static/js/main.a5a73ca3.chunk.js"
+  },
+  {
+    "revision": "8c755b2c1f3aae49cba2",
+    "url": "/Robofriends/./static/js/runtime~main.d6ba24c2.js"
+  }
+]);
