7a4047a59ce823af281d401d0a740dc20ec67ee6
Darshan-Lakade <darshanlakade75@gmail.com>
2019-07-28 17:19:13 +0530

    Adding files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Container/App/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import {createLogger} from 'redux-logger';
+import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { searchRobots, requestRobot } from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const logger = createLogger();
+const rootReducer = combineReducers({searchRobots,requestRobot});
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+                <Provider store={store}>
+                    <App />
+                </Provider>
+, document.getElementById('root'));
+
 serviceWorker.unregister();
